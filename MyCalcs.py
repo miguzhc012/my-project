ed4e37316a30ae7d7182d3437e2add9c4a65d24d
miguzhc012 <migups8@gmail.com>
2024-09-06 19:10:03 -0300

    Calculadoras

---
+++ MyCalcs.py
@@ -0,0 +1,17 @@
+# Demonstração do uso de operadores...
+X = int(input("Digite um número:"))
+Y = int(input("Digite outro número:"))
+
+Soma = X + Y 
+print ("O resultado da soma é: ", Soma)
+
+Subtração = X-Y
+print ("O resultado da subtração é: ", Subtração)
+
+Multiplicação = X * Y
+print ("O resultado da multiplicação é:", Multiplicação)
+
+Divisão = X / Y 
+print ("O resultado da divisão é:", Divisão)
+
+print ("Ok!")
---
+++ MyOperators.py
@@ -0,0 +1,17 @@
+# Demonstração de expreções complexas
+
+# Declaração de variaveis que iremos usar...
+X = 10; Y = 20; Z = 30
+
+Valor = X + (Y / 10 + Z / 10) - 25
+print ("O resultado da expressão #1 é: ", Valor) 
+
+Valor = ((Y - X) + (Z - X)) / 3
+print ("O resultado da expressão #2 é: ", Valor)
+
+Valor = (X + (Y * 2 + (Z / 10))) - 18
+print ("O resultado da expressão #3 é: ", Valor)
+
+# Antes de executar estes codigos, seria
+# possivel calcular "de cabeça" estes valores?
+# Teste de edição...
