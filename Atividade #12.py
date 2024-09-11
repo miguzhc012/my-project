cc4c63bd60f7a9e653ee1e50dac50417e881ad1f
miguzhc012 <migups8@gmail.com>
2024-09-11 20:31:41 -0300

    Atividade do 11/11/2001

--- Atividade #12.py
+++ Atividade #12.py
@@ -6,28 +6,30 @@ print ("Qual e o Clima? 1. Dia ensolarado / 2. Dia nublado / 3. Tempo chuvoso")
 Clima = int (input())
 
 if Loc == 1 and Clima == 1:
-    print ("Você podera ver um eclipse total as 15:25h ")
+    print ("Você podera ver um eclipse total as 15:25h!")
 elif Loc == 1 and Clima == 2:
     print ("Você não consiguira ver o eclipse!") 
 elif Loc == 1 and Clima == 3:
     print ("Você não consiguira ver o eclipse!")
 elif Loc == 2 and Clima == 1:
-    print ("Você podera ver um eclipse parcial por mais ou menos 5 minutos as 15:25h")
+    print ("Você podera ver um eclipse parcial por mais ou menos 5 minutos as 15:25h!")
 elif Loc == 2 and Clima == 2:
     print ("Você não consiguira ver o eclipse!")
 elif Loc == 2 and Clima == 3:
     print ("Você não consiguira ver o eclipse!")
 elif Loc == 3 and Clima == 1:
-    print ("Você podera ver um eclipse parcial por mais ou menos 5 minutos as 15:25h")
+    print ("Você podera ver um eclipse parcial por mais ou menos 5 minutos as 15:25h!")
 elif Loc == 3 and Clima == 2:
     print ("Você não consiguira ver o eclipse!")
 elif Loc == 3 and Clima == 3:
     print ("Você não consiguira ver o eclipse!")
 elif Loc == 4 and Clima == 1:
-    print ("Você podera ver um eclipse parcial por mais ou menos 5 minutos as 15:25h")
+    print ("Você podera ver um eclipse parcial por mais ou menos 5 minutos as 15:25h!")
 elif Loc == 4 and Clima == 2:
     print ("Você não consiguira ver o eclipse!")
 elif Loc == 4 and Clima == 3:
     print ("Você não consiguira ver o eclipse!")
 elif Loc == 5:
-    print ("Você não consiguira ver o eclipse!")                     
+    print ("Você não consiguira ver o eclipse!")
+else:
+    print ("Valor incorreto!")                         
